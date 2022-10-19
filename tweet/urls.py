e2cb5a4034c601886450f89dbb80ec9fd6c632e2
koon <cjstmdgusqw@gmail.com>
2022-10-19 09:46:29 +0900

    게시물 완성

--- tweet/urls.py
+++ tweet/urls.py
@@ -6,6 +6,7 @@ from tweet import views
 urlpatterns = [
     path('write/', views.write, name = 'write'),
     path('community/', views.community, name = 'community'),
-    path('add/<int:id>', views.add, name = 'add')
+    path('community/<int:id>', views.add, name = 'add'),
+    path('community/<int:id>/mod', views.mod)
 ]
 
