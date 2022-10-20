2aa9d60e208ef5d6b0e09ab0e3200f4b1ee34bee
marinred <endrms1995@gmail.com>
2022-10-20 23:30:43 +0900

    Feat: Frontend_Userid_search_apply

--- user/models.py
+++ user/models.py
@@ -4,4 +4,5 @@ from django.conf import settings
 class Users(AbstractUser):
     profile = models.TextField(max_length=500, blank=True)
     follow = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name = 'followed')
+    username = models.CharField(max_length=20)
     
--- user/views.py
+++ user/views.py
@@ -4,7 +4,12 @@ from django.contrib import auth
 
 
     
-def searchname(request, profile):
-    if request.method == "GET":
-        search_name = Users.objects.filter(profile=profile)
-    return render(request, 'search.html', {'search_name':search_name})
+def searchuser(request):
+    if request.method == "POST":
+        search = request.POST.get('searchuser') # 검색어를 serach에 넣은것 # html의 name 동근아!!! 너 미쳤니?? 뒤질 준비해라...
+        searchusers = Users.objects.filter(username=search)
+        print(search)
+        print(searchusers)
+        return render(request, 'searchuser.html', {'searchusers':searchusers}) # .은 확실할 때만 쓰고 남용하지 마라 동근아 제발 . 생활화 하지말자 동근아 한 번 더 기억하자 동근아
+    
+    
