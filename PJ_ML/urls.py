c8a2dc2114d3aa7d9d97b6a0d73af80650434365
marinred <endrms1995@gmail.com>
2022-10-24 19:21:04 +0900

    Make: README.md

--- PJ_ML/urls.py
+++ PJ_ML/urls.py
@@ -8,9 +8,7 @@ urlpatterns = [
     path('user/', include("user.urls")),
     path('tweet/', include('tweet.urls')),
     path('accounts/', include('allauth.urls')),
-
     path('',views.main),
-
     path('searchuser/', views.user_view, name = 'user-list'),
     path('user/follow/<int:id>', views.user_follow, name = 'user-follow'),
 
